848cefe0f66a33f8541873d5cd8ab1fcd7ec07c2
DaniloDiaz35 <82105689+DaniloDiaz35@users.noreply.github.com>
2024-07-11 14:19:36 -0300

    feat: "Controlar color del LED y brillo según la diferencia de temperatura"
    última modificación: eliminar líneas redundantes de código

--- actividades_python/ej_04/main.py
+++ actividades_python/ej_04/main.py
@@ -9,7 +9,7 @@ led_b = PWMLED(27)
 Resistencia_Ref = 10000.0
 Beta_Term = 3900.0 
 
-GAIN = 1
+GAIN_act = 1
 
 def main():
 	while True:
@@ -20,12 +20,12 @@ def main():
         	time.sleep(1)
 
 def read_potentiometer():
-    	value = adc.read_adc(0, gain=GAIN)
+    	value = adc.read_adc(0, gain=GAIN_act)
     	temperature_setpoint = value * 30.0 / 32767.0
     	return temperature_setpoint
 
 def read_thermistor():
-	value = adc.read_adc(1, gain=GAIN)
+	value = adc.read_adc(1, gain=GAIN_act)
 	resistencia = Resistencia_Ref * (32767.0 / value - 1.0)
 	temperature = 1.0 / (1.0 / 298.15 + (1.0 / Beta_Term) * (resistencia / Resistencia_Ref - 1.0)) - 273.15
 	return temperature
